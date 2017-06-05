61d9814296c0bc7051e3d0decbe1a16374e84ba3
Khanh Hua <khanh.hua@axa.com.sg>
2017-06-05 18:27:48 +0800

    Display menu

--- src/ats.erl
+++ src/ats.erl
@@ -6,6 +6,13 @@
 
 -export([start/2, stop/1]).
 -export([simple_loop/1]).
+
+-define(TCP_PORT, 4441).
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+
+-define(SCREEN_MAIN_MENU, 100).
+-define(SCREEN_POST_POSITION, 101).
+-define(SCREEN_SEARCH_POSITIONS, 102).
 %%%===================================================================
 %%% Application callbacks
 %%%===================================================================
@@ -30,6 +37,7 @@ start(_StartType, _StartArgs) ->
   ats_position_service:start_link(),
 
   Pid = spawn(?MODULE, simple_loop, [0]),
+  io:format("ATS Applicant Tracking System initialized...~n"),
   {ok, Pid}.
 
 %%--------------------------------------------------------------------
@@ -50,18 +58,72 @@ stop(_State) ->
 %% Internal
 %%--------------------------------------------------------------------
 simple_loop(State) ->
-  NewState = State + 1,
+  {ok, LSocket} = gen_tcp:listen(?TCP_PORT, ?TCP_OPTIONS),
+  accept(LSocket).
 
+accept(LSocket) ->
+  {ok, Socket} = gen_tcp:accept(LSocket),
+  spawn(fun() -> connection_handler(Socket) end),
+
+  accept(LSocket).
+
+connection_handler(Socket) ->
+  Client = #client{screen = ?SCREEN_MAIN_MENU, socket = Socket},
+  loop(Client).
+
+loop (Client) ->
+  Client2 = ui(Client),
+  loop(Client2).
+  % Ui = ui(Client),
+  % gen_tcp:send(Socket, Ui),
+  % case gen_tcp:recv(Socket, 0) of
+  %   {ok, Data} ->
+  %     gen_tcp:send(Socket, Data),
+  %     connection_handler(Socket);
+  %   {error, closed} ->
+  %     ok
+  % end.
+
+ui(Client) ->
   if
-    State =:= 0 ->
-      io:format("Looping started...~n"),
-      simple_loop(NewState);
-    true ->
-      receive
-        quit -> io:format("Looping terminated~n"), ok
-      after
-        1000 ->
-          io:format("Looping continues...~n"),
-          simple_loop(NewState)
-      end
+    Client#client.screen =:= ?SCREEN_MAIN_MENU ->
+      client_write(Client,
+        "ATS Applicant Tracking System~n"
+        "=============================~n"
+        "1. Search for positions~n"
+        "2. Post a new position~n"
+        "X. Quit~n"
+        "Enter: "),
+      Screen = case client_read(Client) of
+        "1" -> ?SCREEN_SEARCH_POSITIONS;
+        _ -> Client#client.screen
+      end,
+      Client#client{screen = Screen}; % Return a new instance of Client
+    Client#client.screen =:= ?SCREEN_SEARCH_POSITIONS ->
+      client_write(Client,
+        "ATS Applicant Tracking System~n"
+        "=============================~n"
+        "1. Search for positions~n"
+        "-----------------------------~n"
+        "Min Salary: "),
+      MinSalary = client_read(Client),
+      client_write(Client,
+        "Position name: "),
+      PositionName = client_read(Client),
+      io:format("Searching for positions..."),
+      Client#client{screen = ?SCREEN_MAIN_MENU};
+    true -> Client
+  end.
+
+client_write(Client, String) ->
+  gen_tcp:send(Client#client.socket, io_lib:format(String, [])).
+
+client_read(Client) ->
+  case gen_tcp:recv(Client#client.socket, 0) of
+    {ok, Read} ->
+      io:format("Received from client: ~p~n", [Read]),
+      [Ret] = string:tokens(binary_to_list(Read), "\n\r"),
+      io:format("... transformed to: ~p~n", [Ret]),
+      Ret;
+    {error,closed} -> "QUIT"
   end.
